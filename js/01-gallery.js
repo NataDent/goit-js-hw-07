b30f97a46193e91ef717c6ad2e980938ffed6ce8
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-21 13:58:44 +0300

    Додавання модального вікна в процесі

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
-console.log(galleryItems);
-
 const gallery = document.querySelector('.gallery');
-const galleryItems = [];
+
 function createMarkup(arr) {
-    
+    return arr.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('');  
 }
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+gallery.addEventListener('click', handlerClickGallery);
+
+function handlerClickGallery(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(e.target)
+}
+
+// function addModalWindow()
