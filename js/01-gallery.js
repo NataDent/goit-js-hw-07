666c76582daa1dc08339733eaed6f3d998af8d9b
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-22 12:40:16 +0300

    Додала бібліотеку

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ const gallery = document.querySelector('.gallery');
 function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => 
     `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
+  <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
@@ -18,11 +18,16 @@ function createMarkup(arr) {
 gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
 gallery.addEventListener('click', handlerClickGallery);
-
+ 
 function handlerClickGallery(e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log(e.target)
+    console.log(e.target.dataset)
 }
 
+// function findGalleryItem() {
+//     const ({ source }) = item.dataset;
+//    galleryItems.find(item => item.original === source);    
+   
+// }
 // function addModalWindow()
