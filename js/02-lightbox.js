d546d277e3deb2e6479d5cc0d6af77e24985eb79
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-24 00:44:01 +0300

    Task 2 is in progress

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({ preview, original, description }) => 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('');  
+}
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+// gallery.addEventListener('click', handlerClickGallery);
+
