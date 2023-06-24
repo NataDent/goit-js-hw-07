7abed07221bd3a74956acd27173947ef81f4ebc4
Наталья ЧеркашинаКоцько <cherkashyna.n@gmail.com>
2023-06-24 21:57:53 +0300

    Finished task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,11 +8,22 @@ function createMarkup(arr) {
     return arr.map(({ preview, original, description }) => 
     `<li class="gallery__item">
    <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
+      <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
    </a>
 </li>`).join('');  
 }
 gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
-// gallery.addEventListener('click', handlerClickGallery);
+gallery.addEventListener('click', handlerClickGallery);
+
+function handlerClickGallery(e) {
+   e.preventDefault();
+   if (!e.target ) {
+      return;
+   }
+      console.log(e.target);
+   // var lightbox = $('.gallery a').simpleLightbox({ captionDelay: 250 });
+   var lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+   return lightbox;
+}
 
